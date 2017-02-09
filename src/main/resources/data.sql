5074ef56f09a169ef08000031f904bdc70c44af7
gurkancakir <gurkan.cakir@mimcrea.com>
2017-02-09 15:59:19 +0300

    sql sorgusu duzenlendi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into app_user(id, password, username) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'test@gmail.com');
+insert into app_user(id, password, username, price, weekly_working_hours) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'test@gmail.com', 0, 0);
 insert into user_role(app_user_id, role) values(1, 'ADMIN');
 insert into user_role(app_user_id, role) values(1, 'PREMIUM_MEMBER');
