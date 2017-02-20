2ca48e8300e210878a35553e9a37037d1ef71a0f
Gurkan Cakir <gurkan.cakir@engineer.com>
2017-02-20 23:42:41 +0300

    user methodlari yazildi

--- src/main/java/com/gurkan/web/AdminController.java
+++ src/main/java/com/gurkan/web/AdminController.java
@@ -4,6 +4,7 @@ import com.gurkan.entity.User;
 import com.gurkan.security.auth.JwtAuthenticationToken;
 import com.gurkan.service.DatabaseUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -48,10 +49,42 @@ public class AdminController {
     public User createUser(String username, String password, Integer price, Integer weeklyWorkingHours) {
         price = (price == null) ? 0 : price;
         weeklyWorkingHours = (weeklyWorkingHours == null) ? 0 : weeklyWorkingHours;
-
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        password = bCryptPasswordEncoder.encode(password);
         User user = new User(username, password, price, weeklyWorkingHours);
         return databaseUserService.getUserRepository().save(user);
     }
 
+    /*
+    *   description : Get User
+    *   method      : GET
+    *   required    : String username
+    *
+    *   @Author Gurkan CAKIR
+    *
+    * */
+    @RequestMapping(value="/api/user", method=RequestMethod.GET)
+    @ResponseBody
+    public User getUser(String username) {
+        return databaseUserService.getUserRepository().findByUsername(username).orElse(new User());
+    }
+
+
+    /*
+    *   description : Delete User
+    *   method      : DELETE
+    *   required    : String username
+    *
+    *   @Author Gurkan CAKIR
+    *
+    * */
+    @RequestMapping(value="/api/user", method=RequestMethod.DELETE)
+    @ResponseBody
+    public User deleteUser(String username) {
+        User user =  databaseUserService.getUserRepository().findByUsername(username).orElse(new User());
+        if (user.getId() != null)
+            databaseUserService.getUserRepository().delete(user);
+        return user;
+    }
 
 }
