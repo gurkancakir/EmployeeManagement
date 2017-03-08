0893625dbda0658789051e3ad31b1553f99ba0da
Gurkan Mustafa Cakir <gurkancakir@Gurkan-MacBook-Pro.local>
2017-03-08 20:01:32 +0300

    department service duzeltildi

--- src/main/java/com/gurkan/service/DatabaseDepartmentService.java
+++ src/main/java/com/gurkan/service/DatabaseDepartmentService.java
@@ -1,20 +1,30 @@
 package com.gurkan.service;
 
+import com.gurkan.entity.Department;
 import com.gurkan.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Created by gurkan on 08.03.2017.
  */
+@Service
 public class DatabaseDepartmentService {
-    private final DepartmentRepository departmentRepository;
 
     @Autowired
-    public DatabaseDepartmentService(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
+    private DepartmentRepository departmentRepository;
+
+    public Department save(Department department) {
+        return departmentRepository.save(department);
     }
 
-    public DepartmentRepository getDepartmentRepository() {
-        return departmentRepository;
+    public Optional<Department> findByName(String name) {
+        return departmentRepository.findByName(name);
+    }
+
+    public void delete(Department department) {
+        departmentRepository.delete(department);
     }
 }
